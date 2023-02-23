27378ece0bd479208c952ac8c56c65fa1986ef83
Nadarajan GS <nadarajangs31@gmail.com>
2023-02-23 05:47:26 +0000

    view cart btn added

--- libs/console.log.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Console{
-    public static function log($arr){
-        $arr = json_encode($arr);
-        echo "<script> console.log($arr); </script>";
-    }
-}
--- libs/load.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "config.php";
-include "session.class.php";
-include "filter.class.php";
-include "db.class.php";
-include "console.log.php";
-
-session::start();
